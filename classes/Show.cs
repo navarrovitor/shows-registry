fb74247b918fa9c92eea24db78622b092f2c92a2
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-09-18 11:59:11 -0300

    implement show methods

--- classes/Show.cs
+++ classes/Show.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace shows_registry
 {
   public class Show : BaseEntity
@@ -6,6 +8,7 @@ namespace shows_registry
     private string Title { get; set; }
     private string Description { get; set; }
     private int Year { get; set; }
+    private bool Erasen { get; set; }
     public Show(int id, Genre genre, string title, string description, int year)
     {
       this.Id = id;
@@ -13,8 +16,38 @@ namespace shows_registry
       this.Title = title;
       this.Description = description;
       this.Year = year;
+      this.Erasen = false;
     }
+
+    public override string ToString()
+    {
+      string returnMessage = "";
+      returnMessage += "Genre: " + this.Genre + Environment.NewLine;
+      returnMessage += "Title: " + this.Title + Environment.NewLine;
+      returnMessage += "Description: " + this.Description + Environment.NewLine;
+      returnMessage += "Year: " + this.Year + Environment.NewLine;
+      returnMessage += "Erasen: " + this.Erasen;
+      return returnMessage;
+    }
+
+    public int returnId()
+    {
+      return this.Id;
+    }
+
+    public string returnTitle()
+    {
+      return this.Title;
+    }
+
+    public bool returnErasen()
+    {
+      return this.Erasen;
+    }
+    public void Erase()
+    {
+      this.Erasen = true;
+    }
+
   }
-
-
 }
